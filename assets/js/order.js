0e19f3448cc04c93edd3de84e42500115b9e500a
danielw12 <33023142+danielw12@users.noreply.github.com>
2019-07-08 16:31:37 +0200

    podstrony

---
+++ assets/js/order.js
@@ -0,0 +1,46 @@
+$('.check-box :radio').change(function () {
+    $(this).closest('.check-box').toggleClass('checked', this.checked);
+});
+
+
+$(document).ready(function () {
+    $('div.check-box input[type="radio"]').click(function () {
+        $('div.check-box').removeClass('checked');
+
+    });
+});
+
+var ele = document.getElementsByClassName('iradio');
+
+function displayRadioValue() {
+    document.getElementById("result").innerHTML = "";
+    
+
+    for (i = 0; i < ele.length; i++) {
+
+        if (ele[i].type = "radio") {
+
+            if (ele[i].checked)
+                document.getElementById("result").innerHTML += ele[i].value + " zł";
+        }
+    }
+}
+
+var olo = document.getElementsByClassName('radio2');
+
+function displayTransport() {
+    document.getElementById("result2").innerHTML = "";
+    
+
+    for (i = 0; i < olo.length; i++) {
+
+        if (olo[i].type = "radio") {
+
+            if (olo[i].checked)
+                document.getElementById("result2").innerHTML += olo[i].value + " zł";
+        }
+    }
+}
+
+
+
